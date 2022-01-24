fbd7284eb377473ddff8eb92b54bed8916ef0bea
Érico Monteiro <erico.cintra@gmail.com>
2022-01-24 02:10:32 -0300

    create project

---
+++ src/test/kotlin/ericomonteiro/com/github/circuitbreaker/CircuitBreakerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ericomonteiro.com.github.circuitbreaker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
